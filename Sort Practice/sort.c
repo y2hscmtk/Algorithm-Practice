b0c88f6173c47b2dffbb3726dc0c6a12429a2cb2
Y2K <y2_12@naver.com>
2022-11-17 22:48:33 +0900

    bubble sort

--- Sort Practice/sort.c
+++ Sort Practice/sort.c
@@ -43,28 +43,62 @@ void insertion_sort(int list[], int n)
 	}
 }
 
+
+//버블 정렬 => 최악의 경우와 최선의 경우가 모두 동일하게 O(n2)의 시간복잡도를 보장한다.
+//레코드의 이동연산이 비교연산보다 더 많은 시간을 소요한다.
+void bubble_sort(int list[], int n) //n은 리스트 요소의 '개수'를 의미한다. (10개) => 마지막 인덱스 넘버 n-1
+{
+	//인접한 2개의 레코드를 비교하여 순서대로 되어 있지 않으면 서로 교환
+	//한번의 과정을 거치면 맨 오른쪽에 가장 큰 값이 위치하게 됨(당연하게도)
+	//이러한 과정을 왼쪽부터 오른쪽까지 버블모양으로 반복
+	
+	int i, j, temp;
+	for (i = n - 1; i > 0; i--) { 
+		for (j = 0; j < i; j++) {  // 앞뒤의 레코드를 비교한 후 교체
+			if (list[j] > list[j + 1])  //앞뒤의 값을 비교하여 더 큰 값을 오른쪽으로
+				SWAP(list[j], list[j + 1], temp);
+		}
+	}
+}
+
+
+//배열을 랜덤한 값으로 초기화 한다.
+void init(int list[], int n) {
+	//초기화
+	for (int i = 0; i < n; i++)// 난수 생성 및 출력 
+		list[i] = rand() % 100; // 난수 발생 범위 0~99
+}
+
+//배열을 화면에 출력한다.
+void printArray(int list[], int n) {
+	for (int i = 0; i < n; i++)
+		printf("%d ", list[i]);
+	printf("\n\n");
+}
+
 int main(void)
 {
 	int i, n;
 	n = MAX_SIZE;
 	int list[MAX_SIZE];
 	srand(time(NULL));
-	for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-		list[i] = rand() % 100; // 난수 발생 범위 0~99
+
+	init(list, n); //랜덤값 삽입
 
 	printf("선택 정렬\n");
 	selection_sort(list, n); // 선택정렬 호출 
-	for (i = 0; i < n; i++)
-		printf("%d ", list[i]);
+	printArray(list, n); //배열 출력
 
-	//초기화
-	for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
-		list[i] = rand() % 100; // 난수 발생 범위 0~99
+	init(list, n); //랜덤값 삽입
 
-	printf("\n삽입 정렬\n");
+	printf("삽입 정렬\n");
 	insertion_sort(list, n);
-	for (i = 0; i < n; i++)
-		printf("%d ", list[i]);
+	printArray(list, n); //배열 출력
+
+	init(list, n); //랜덤값 삽입
+	bubble_sort(list, n);
+	printArray(list, n); //배열 출력
+
 	printf("\n");
 	return 0;
 }
